daae8e1c801aa96c1ce1805ad1580385560282e7
1Salivan1 <eugenysalivan@gmail.com>
2025-02-04 23:30:22 +0200

    fixed issues, added modal

--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,54 @@
-const body = document.querySelector('body')
-const popup = document.querySelector('.popup-country');
-const btn = document.querySelector('.country');
-const popupClose = document.querySelector('.popup-country__button');
+const body = document.querySelector("body");
+const popup = document.querySelector(".popup-country");
+const btn = document.querySelector(".country");
+const popupClose = document.querySelector(".popup-country__button");
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    body.style.overflow = "hidden";
-    popup.classList.add('active');
-})
+// Country modal
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  body.style.overflow = "hidden";
+  popup.classList.add("active");
+});
 
-popupClose.addEventListener('click', () => {
+popupClose.addEventListener("click", () => {
+  body.style.overflow = "visible";
+  popup.classList.remove("active");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == popup) {
     body.style.overflow = "visible";
-    popup.classList.remove('active');
-})
+    popup.classList.remove("active");
+  }
+});
 
-window.addEventListener('click', (e) => {
-    if (e.target == popup) {
-        body.style.overflow = "visible";
-        popup.classList.remove('active');
-    }
-})
+// Offer modal
+const orderButton = document.querySelector(".main__btn");
+const popupOrder = document.querySelector(".popup-order");
+const closeOrderButton = document.querySelector(".popup-order__close-btn");
+const orderForm = document.getElementById("popup-order__form");
+
+orderButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  popupOrder.style.display = "flex";
+});
+
+closeOrderButton.addEventListener("click", () => {
+  popupOrder.style.display = "none";
+});
+
+popupOrder.addEventListener("click", (e) => {
+  if (e.target === popupOrder) {
+    popupOrder.style.display = "none";
+  }
+});
+
+orderForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  document.querySelector(".popup-order").style.display = "none";
+
+  alert(
+    "Ваша заявка в обработке, ожидайте, мы свяжемся с вами в течении недели!"
+  );
+});
